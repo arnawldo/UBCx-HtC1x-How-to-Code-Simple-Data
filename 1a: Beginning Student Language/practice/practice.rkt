6bfed6f19f81acdc50553b333b9e687ad5ac62d1
Arnold Taremwa <taremwaarnold@gmail.com>
2017-06-06 17:11:40 +0300

    added practice files for 1a

---
+++ 1a: Beginning Student Language/practice/practice.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname practice) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;(+ 3 4)
+;
+;(/ 12 (* 2 3))
+(/ 3 (+ 6.2 -12 4))
+
+(/ (+ -8 6.2) 3)
+
+-0.6
+
+"apple"
+
+(string-append "Ada" " " "Lovelace")
+
+(string-length "apple")
+
+(substring "Caribou" 2 4)
